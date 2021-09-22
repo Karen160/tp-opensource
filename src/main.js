bad85513de14eb30837d8c0d5cce413d65b68c3f
CocsAncel <corentin.ancel@edu.devinci.fr>
2021-09-22 09:54:18 +0200

    ADD : HTML and JS files

---
+++ src/main.js
@@ -0,0 +1,44 @@
+const fetchButton = document.getElementById("fetch-button");
+const api_key = "66e2c2bd815945a7860144831212109";
+const addressInput = document.getElementById("address-input");
+const cityInput = document.getElementById("city-input");
+const codeInput = document.getElementById("code-input");
+
+async function getAddressLocation() {
+  let addressValue = addressInput.value;
+  let cityValue = cityInput.value;
+  let codeValue = codeInput.value;
+
+  const addressLocationData = await fetch(
+    "https://api-adresse.data.gouv.fr/search/?q=" +
+      addressValue +
+      cityValue +
+      "$postcode=" +
+      codeValue
+  );
+
+  const addressLocation = await addressLocationData.json();
+
+  return addressLocation.features[0].geometry.coordinates;
+}
+
+async function getWeatherData(coords) {
+  const weatherData = await fetch(
+    "http://api.weatherapi.com/v1/current.json?key=" +
+      api_key +
+      "&q=" +
+      coords[1] +
+      "," +
+      coords[0]
+  );
+
+  const weatherDataResponse = await weatherData.json();
+
+  return weatherDataResponse.current;
+}
+
+async function getData() {
+  console.log(await getWeatherData(await getAddressLocation()));
+}
+
+fetchButton.addEventListener("click", getData);
