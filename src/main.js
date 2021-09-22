3bffaeeb80480eea261731cd447c9e5914837780
CocsAncel <corentin.ancel@edu.devinci.fr>
2021-09-22 10:20:41 +0200

    ADD : DOM changes to show weather data

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-const fetchButton = document.getElementById("fetch-button");
 const api_key = "66e2c2bd815945a7860144831212109";
+
+const fetchButton = document.getElementById("fetch-button");
 const addressInput = document.getElementById("address-input");
 const cityInput = document.getElementById("city-input");
 const codeInput = document.getElementById("code-input");
+
+const cloudParagraph = document.getElementById("cloud");
+const humidityParagraph = document.getElementById("humidity");
+const lastUpdatedParagraph = document.getElementById("last-updated");
+const temperatureParagraph = document.getElementById("temperature");
+const windDegreeParagraph = document.getElementById("wind-degree");
+const windKmhParagraph = document.getElementById("wind-kmh");
 
 async function getAddressLocation() {
   let addressValue = addressInput.value;
@@ -38,7 +46,16 @@ async function getWeatherData(coords) {
 }
 
 async function getData() {
-  console.log(await getWeatherData(await getAddressLocation()));
+  let data = await getWeatherData(await getAddressLocation());
+
+  console.log(data);
+
+  cloudParagraph.innerHTML += data.cloud;
+  humidityParagraph.innerHTML += data.humidity;
+  lastUpdatedParagraph.innerHTML += data.last_updated;
+  temperatureParagraph.innerHTML += data.temp_c;
+  windDegreeParagraph.innerHTML += data.wind_degree;
+  windKmhParagraph.innerHTML += data.wind_kph;
 }
 
 fetchButton.addEventListener("click", getData);
