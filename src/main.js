4c0e7b83e5d6e694566ded7ddf9fa96ffe94716e
Benjamin B <Benjamin.burstein@edu.devinci.fr>
2021-09-22 10:43:43 +0200

    validator adress or city not empty

--- src/main.js
+++ src/main.js
@@ -4,41 +4,51 @@ const addressInput = document.getElementById("address-input");
 const cityInput = document.getElementById("city-input");
 const codeInput = document.getElementById("code-input");
 
-async function getAddressLocation() {
-  let addressValue = addressInput.value;
-  let cityValue = cityInput.value;
-  let codeValue = codeInput.value;
 
-  const addressLocationData = await fetch(
-    "https://api-adresse.data.gouv.fr/search/?q=" +
-      addressValue +
-      cityValue +
-      "$postcode=" +
-      codeValue
-  );
+  async function getAddressLocation() {
+    let addressValue = addressInput.value;
+    let cityValue = cityInput.value;
+    let codeValue = codeInput.value;  
 
-  const addressLocation = await addressLocationData.json();
+    const addressLocationData = await fetch(
+      "https://api-adresse.data.gouv.fr/search/?q=" +
+        addressValue +
+        cityValue +
+        "$postcode=" + codeValue 
+    );
+  
+    const addressLocation = await addressLocationData.json();
+    return addressLocation.features[0].geometry.coordinates;
 
-  return addressLocation.features[0].geometry.coordinates;
-}
-
-async function getWeatherData(coords) {
-  const weatherData = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=" +
-      api_key +
-      "&q=" +
-      coords[1] +
-      "," +
-      coords[0]
-  );
-
-  const weatherDataResponse = await weatherData.json();
-
-  return weatherDataResponse.current;
-}
+  }
+  
+  async function getWeatherData(coords) {
+    const weatherData = await fetch(
+      "http://api.weatherapi.com/v1/current.json?key=" +
+        api_key +
+        "&q=" +
+        coords[1] +
+        "," +
+        coords[0]
+    );
+  
+    const weatherDataResponse = await weatherData.json();
+  
+    return weatherDataResponse.current;
+  }
 
 async function getData() {
-  console.log(await getWeatherData(await getAddressLocation()));
+  let addressValue = addressInput.value;
+  let cityValue = cityInput.value;  
+
+  if(addressValue != "" || cityValue != "") {
+    document.getElementById("error").style.display = "none";
+    console.log(await getWeatherData(await getAddressLocation()));
+  }else{
+    document.getElementById("error").style.display = "flex";
+  }
+
 }
 
-fetchButton.addEventListener("click", getData);
+  fetchButton.addEventListener("click", getData);
+  
